e524d0a53833d7b3eecabd275bfe27bb56d2c35b
Jason Faas <5844854+JasonFaas@users.noreply.github.com>
2018-11-21 10:54:19 -0800

    Initial files

---
+++ Phantom/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "\nHello, World!" << std::endl;
+    std::cout << "\n\nHello, World Again!" << std::endl;
+	std::cin.ignore();
+    return 0;
+}
